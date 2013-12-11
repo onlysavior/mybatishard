060d9d3168f724f86f89cdee852ea77052d57228
onlysavior@foxmail.com <onlysavior@foxmail.com>
2013-12-11 17:50:48 +0800

    use SQLParser to parse the SQL

--- src/main/java/org/makersoft/shards/utils/RuleUtil.java
+++ src/main/java/org/makersoft/shards/utils/RuleUtil.java
@@ -1,9 +1,25 @@
 package org.makersoft.shards.utils;
 
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.expr.SQLIdentifierExpr;
+import com.alibaba.druid.sql.ast.statement.SQLExprTableSource;
+import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
+import com.alibaba.druid.sql.ast.statement.SQLTableSource;
+import com.alibaba.druid.sql.ast.statement.SQLTableSourceImpl;
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.*;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlASTVisitorAdapter;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlOutputVisitor;
+import com.alibaba.druid.sql.parser.SQLParserUtils;
+import com.alibaba.druid.sql.parser.SQLStatementParser;
+import com.alibaba.druid.sql.visitor.SQLASTOutputVisitorUtils;
+import com.alibaba.druid.sql.visitor.SQLASTVisitor;
+import com.alibaba.druid.util.JdbcUtils;
 import org.makersoft.shards.rule.impl.VirtualTable;
-import org.springframework.util.StringUtils;
 
-import static org.makersoft.shards.session.impl.ShardedSqlSessionImpl.*;
+import java.util.List;
+
+import static org.makersoft.shards.session.impl.ShardedSqlSessionImpl.extractId;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,22 +35,51 @@ public class RuleUtil {
     public static String replcePlaceHolder(String sql,
                                     VirtualTable rule,
                                     Object parameter) {
-        //FIXME really need is a way to parse SQL 92
-        int begin = sql.indexOf(SPLIT_BEGIN);
-        int end = sql.substring(begin).indexOf(SPLIT_END) + begin + 2;
-        if(begin == -1 || end == -1) {
-            return sql;
+        String actualTableName = rule.getPhysicsTableIndex(parameter);
+        RuleSQLVisitor ruleSQLVisitor = new RuleSQLVisitor(actualTableName);
+
+        List<SQLStatement> statements = SQLUtils.parseStatements(sql, JdbcUtils.MYSQL);
+        StringBuilder sb = new StringBuilder();
+        MySqlOutputVisitor mySqlOutputVisitor = new MySqlOutputVisitor(sb);
+        for(SQLStatement s : statements) {
+            s.accept(ruleSQLVisitor);
+            s.accept(mySqlOutputVisitor);
         }
 
-        String toReplce = sql.substring(begin, end);
-        String actualTableName = rule.getPhysicsTableIndex(parameter);
+        return sb.toString();
 
-
-        return sql.replace(toReplce, actualTableName);
     }
 
-    public static String extractActualTableName(VirtualTable rule, Object parameter) {
-        Object value =  extractId(parameter);
-        return rule.getPhysicsTableIndex(value);
+    private static class RuleSQLVisitor extends MySqlASTVisitorAdapter {
+        private final String actualName;
+
+        public RuleSQLVisitor(String actualName) {
+            this.actualName = actualName;
+        }
+
+        @Override
+        public boolean visit(MySqlSelectQueryBlock x) {
+            x.setFrom(new SQLExprTableSource(new SQLIdentifierExpr(actualName)));
+            return super.visit(x);
+        }
+
+        @Override
+        public boolean visit(MySqlDeleteStatement x) {
+           x.getFrom().setAlias(actualName);
+           return super.visit(x);
+        }
+
+        @Override
+        public boolean visit(MySqlUpdateStatement x) {
+            x.setTableSource(new SQLExprTableSource(new SQLIdentifierExpr(actualName)));
+            return super.visit(x);
+        }
+
+        @Override
+        public boolean visit(MySqlInsertStatement x) {
+            x.setTableName(new SQLIdentifierExpr(actualName));
+           return super.visit(x);
+        }
     }
+
 }
